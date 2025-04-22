675b144f0a2407e635ea50fe8404788c618e7ef3
pergunu <pergunusitubondo@gmail.com>
2025-04-22 21:52:25 +0700

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-// script.js FINAL
-// Logika lengkap quiz interaktif AI
-// Fitur: Soal acak, identitas, AI integrasi, feedback lucu, animasi skor, timer, adzan, dll.
-// [Kode lengkap diisi di sini - dikembangkan dalam tahap lanjut]
