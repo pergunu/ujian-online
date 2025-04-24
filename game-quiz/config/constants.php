33882b88ffcafaf9bf111388682369ed1b651af2
pergunu <pergunusitubondo@gmail.com>
2025-04-24 21:48:59 +0700

    Create constants.php

---
+++ game-quiz/config/constants.php
@@ -0,0 +1,9 @@
+<?php
+// Credential Default
+define('DEFAULT_USER', 'Cendhanu');
+define('DEFAULT_PASS', '1499');
+define('ADMIN_WHATSAPP', '6287865614222');
+
+// OTP Settings
+define('OTP_EXPIRY', 300); // 5 menit
+define('OTP_LENGTH', 4); // 4 digit
