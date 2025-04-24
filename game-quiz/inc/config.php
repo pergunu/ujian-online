baf817e02b1df2a3847d59612f6ce0ddaf141d07
pergunu <pergunusitubondo@gmail.com>
2025-04-24 22:44:58 +0700

    Create config.php

---
+++ game-quiz/inc/config.php
@@ -0,0 +1,10 @@
+<?php
+// Credential Default
+define('DEFAULT_USER', 'Cendhanu');
+define('DEFAULT_PASS', '1499');
+define('DEFAULT_HP', '6287865614222'); 
+
+// Pengaturan OTP
+define('OTP_EXPIRY', 300); // OTP berlaku 5 menit
+session_start();
+?>
