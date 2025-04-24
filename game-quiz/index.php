884bb1b40652fc411c2f4bbb6d23726f407d6ec5
pergunu <pergunusitubondo@gmail.com>
2025-04-24 22:51:33 +0700

    Update index.php

--- game-quiz/index.php
+++ game-quiz/index.php
@@ -1,21 +1,9 @@
 <?php
-session_start();
 require 'inc/config.php';
-
-// Cek apakah user sudah login
 if (!isset($_SESSION['logged_in'])) {
     header("Location: login.php");
     exit;
 }
 ?>
-<!DOCTYPE html>
-<html lang="id">
-<head>
-    <!-- Meta dan CSS tetap sama -->
-    <?php include 'inc/security.php'; ?>
-</head>
-<body>
-    <!-- Konten quiz Anda yang sudah ada -->
-    <script src="assets/js/script.js"></script>
-</body>
-</html>
+<!-- Konten quiz Anda yang sudah ada -->
+<script src="assets/js/script.js"></script>
