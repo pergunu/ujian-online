11f5b8282d923c359b1c5e613989970b5461d1b3
pergunu <pergunusitubondo@gmail.com>
2025-04-24 22:48:45 +0700

    Update login.php

--- game-quiz/login.php
+++ game-quiz/login.php
@@ -1,24 +1,8 @@
-<?php
-session_start();
-if (isset($_SESSION['logged_in'])) {
-    header("Location: index.php");
-    exit;
-}
-?>
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Login - QUIZ PERGUNU</title>
-    <link rel="stylesheet" href="assets/css/style.css">
-</head>
-<body>
-    <div class="login-container">
-        <form id="loginForm" action="process/login.php" method="POST">
-            <input type="text" name="username" placeholder="Username" required>
-            <input type="password" name="password" placeholder="Password" required>
-            <button type="submit">Login</button>
-        </form>
-    </div>
-    <script src="assets/js/script.js"></script>
-</body>
-</html>
+<form id="loginForm" action="process/login.php" method="POST">
+    <input type="text" name="username" placeholder="Username" required>
+    <input type="password" name="password" placeholder="Password" required>
+    <button type="submit">Login</button>
+    <?php if (isset($_GET['error'])): ?>
+        <p class="error">Login gagal!</p>
+    <?php endif; ?>
+</form>
