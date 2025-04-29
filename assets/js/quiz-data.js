4d32edaaec3cc13ffe0c74902b7cfaba304a502a
pergunu <pergunusitubondo@gmail.com>
2025-04-29 16:53:32 +0700

    Update quiz-data.js

--- assets/js/quiz-data.js
+++ assets/js/quiz-data.js
@@ -1,7 +1,5 @@
-const questions = [
-  {
-    question: "Pertanyaan baru?",
-    options: ["A. ...", "B. ...", "C. ...", "D. ...", "E. ..."],
-    answer: 0
-  }
-];
+{
+  question: "Pertanyaan?",
+  options: ["A. ...", "B. ...", "C. ...", "D. ...", "E. ..."],
+  answer: 0 // Index jawaban benar (0 untuk A, 1 untuk B, dst)
+}
