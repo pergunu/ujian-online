75ded539f5c00b5f6a8253f52bce97df87727dad
pergunu <pergunusitubondo@gmail.com>
2025-04-29 10:09:18 +0700

    Create questions.js

---
+++ assets/js/questions.js
@@ -0,0 +1,23 @@
+// questions.js
+const questions = {
+    dasar: [
+        {
+            question: "Siapa pendiri NU?",
+            options: [
+                "A. KH. Hasyim Asy'ari", 
+                "B. KH. Ahmad Dahlan", 
+                "C. Buya Hamka"
+            ],
+            answer: 0
+        },
+        {
+            question: "Tahun berapa NU didirikan?",
+            options: [
+                "A. 1926", 
+                "B. 1945", 
+                "C. 1938"
+            ],
+            answer: 0
+        }
+    ]
+};
