dc8caecb0a1d772af8b52a8ed038f1836d17ec76
pergunu <pergunusitubondo@gmail.com>
2025-04-30 14:05:21 +0700

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,25 +1,9 @@
-import { CONFIG } from './config.js';
-
-export class Auth {
-  static isAdminAuthenticated() {
-    return localStorage.getItem('admin_auth') === CONFIG.ADMIN_PASSWORD;
-  }
-
-  static authenticate(password) {
-    if (password === CONFIG.ADMIN_PASSWORD) {
-      localStorage.setItem('admin_auth', password);
-      return true;
+export default class Auth {
+    login(username, password) {
+        // Logika login
     }
-    return false;
-  }
 
-  static logout() {
-    localStorage.removeItem('admin_auth');
-  }
-
-  static checkAuth() {
-    if (!this.isAdminAuthenticated()) {
-      window.location.href = '/';
+    logout() {
+        // Logika logout
     }
-  }
 }
