80efc392a8442ffe2910ac301accbf8abd5322cc
pergunu <pergunusitubondo@gmail.com>
2025-04-30 10:26:12 +0700

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,25 @@
+import { CONFIG } from './config.js';
 
+export class Auth {
+  static isAdminAuthenticated() {
+    return localStorage.getItem('admin_auth') === CONFIG.ADMIN_PASSWORD;
+  }
+
+  static authenticate(password) {
+    if (password === CONFIG.ADMIN_PASSWORD) {
+      localStorage.setItem('admin_auth', password);
+      return true;
+    }
+    return false;
+  }
+
+  static logout() {
+    localStorage.removeItem('admin_auth');
+  }
+
+  static checkAuth() {
+    if (!this.isAdminAuthenticated()) {
+      window.location.href = '/';
+    }
+  }
+}
