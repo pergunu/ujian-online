66a0da6c2db4e23c5456368db1db2c3f6239c850
cendhanu <pergunusitubondo@gmail.com>
2025-05-15 21:47:20 +0700

    Update app.js

--- js/core/app.js
+++ js/core/app.js
@@ -1,3 +1,5 @@
+// app.js
+
 let currentCategory = '';
 let currentSubcategory = '';
 let currentLevel = '';
@@ -5,17 +7,17 @@ let score = 0;
 let quizStartTime = null;
 
 function formatName(name) {
-  return name.split(' ')
-            .map(word => word.charAt(0).toUpperCase() + word.slice(1))
-            .join(' ');
+    return name.split(' ')
+              .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+              .join(' ');
 }
 
 function generateUniqueCode() {
-  const chars = 'ABCDEFGHJKLMNPQRSTUVWXYZ23456789';
-  let code = '';
-  for (let i = 0; i < 8; i++) {
-    if (i > 0 && i % 4 === 0) code += '-';
-    code += chars[Math.floor(Math.random() * chars.length)];
-  }
-  return code;
+    const chars = 'ABCDEFGHJKLMNPQRSTUVWXYZ23456789';
+    let code = '';
+    for (let i = 0; i < 8; i++) {
+        if (i > 0 && i % 4 === 0) code += '-';
+        code += chars[Math.floor(Math.random() * chars.length)];
+    }
+    return code;
 }
