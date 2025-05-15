af859158cf60defb50e41004d72cc9efd37fc48c
cendhanu <pergunusitubondo@gmail.com>
2025-05-15 21:36:36 +0700

    Update admin.js

--- admin/admin.js
+++ admin/admin.js
@@ -1,37 +1,27 @@
 document.addEventListener('DOMContentLoaded', () => {
     loadEnabledCategories();
     updateCategoryUI();
-
-    document.getElementById('togglePelajar').addEventListener('change', function () {
-        enabledCategories.pelajar = this.checked;
-        updateCategoryUI();
-        saveEnabledCategories();
-    });
-
-    document.getElementById('toggleUmum').addEventListener('change', function () {
-        enabledCategories.umum = this.checked;
-        updateCategoryUI();
-        saveEnabledCategories();
-    });
-
-    document.querySelectorAll('.toggle-subcategory').forEach(checkbox => {
-        checkbox.addEventListener('change', function () {
-            const category = this.dataset.category;
-            const subcategory = this.dataset.subcategory;
-            enabledCategories.subcategories[category][subcategory] = this.checked;
-            updateCategoryUI();
-            saveEnabledCategories();
-        });
-    });
-
-    document.getElementById('disableAllPelajarBtn').addEventListener('click', () => {
-        if (confirm('Nonaktifkan semua subkategori pelajar?')) {
-            Object.keys(enabledCategories.subcategories.pelajar).forEach(sub => {
-                enabledCategories.subcategories.pelajar[sub] = false;
-            });
-            updateCategoryUI();
-            saveEnabledCategories();
-            alert("Semua subkategori pelajar telah dinonaktifkan.");
-        }
-    });
 });
+
+function enableCategory(category, isChecked) {
+    enabledCategories[category] = isChecked;
+    updateCategoryUI();
+    saveEnabledCategories();
+}
+
+function enableSubcategory(category, subcategory, isChecked) {
+    enabledCategories.subcategories[category][subcategory] = isChecked;
+    updateCategoryUI();
+    saveEnabledCategories();
+}
+
+function disableAllSubcategories(category) {
+    if (confirm(`Nonaktifkan semua subkategori ${category}?`)) {
+        Object.keys(enabledCategories.subcategories[category]).forEach(sub => {
+            enabledCategories.subcategories[category][subcategory] = false;
+        });
+        updateCategoryUI();
+        saveEnabledCategories();
+        alert(`Semua subkategori ${category} telah dinonaktifkan.`);
+    }
+}
