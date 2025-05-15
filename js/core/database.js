264cbb520d4387ccd0acb9e130540348f628f5a0
cendhanu <pergunusitubondo@gmail.com>
2025-05-15 21:47:54 +0700

    Update database.js

--- js/core/database.js
+++ js/core/database.js
@@ -1,21 +1,58 @@
+// database.js
+
 const questions = {
-  pelajar: {
-    ipa: {
-      sd: [],
-      smp: [],
-      sma: []
+    pelajar: {
+        ipa: {
+            sd: [],
+            smp: [],
+            sma: []
+        },
+        ips: {
+            sd: [],
+            smp: [],
+            sma: []
+        },
+        matematika: {
+            sd: [],
+            smp: [],
+            sma: []
+        },
+        agama: {
+            sd: [],
+            smp: [],
+            sma: []
+        },
+        ppkn: {
+            sd: [],
+            smp: [],
+            sma: []
+        },
+        sejarah: {
+            smp: [],
+            sma: []
+        },
+        bahasa_indonesia: {
+            sd: [],
+            smp: [],
+            sma: []
+        },
+        bahasa_inggris: {
+            sd: [],
+            smp: [],
+            sma: []
+        }
     },
-    ips: {
-      sd: [],
-      smp: [],
-      sma: []
+    umum: {
+        logika: {
+            sd: [],
+            smp: [],
+            sma: []
+        },
+        swasta: {
+            umum: []
+        },
+        pemerintah: {
+            umum: []
+        }
     }
-  },
-  umum: {
-    logika: {
-      sd: [],
-      smp: [],
-      sma: []
-    }
-  }
 };
