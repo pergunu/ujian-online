e83b560bcaf9834750edc4a9b1770ed6f99a34d7
cendhanu <pergunusitubondo@gmail.com>
2025-05-15 20:05:30 +0700

    Update admin-panel.js

--- js/admin-panel.js
+++ js/admin-panel.js
@@ -1,47 +1,13 @@
-// Event listener untuk toggle kategori utama
+// admin-panel.js
+
 document.getElementById('togglePelajar').addEventListener('change', function () {
-  enabledCategories.pelajar = this.checked;
-  updateCategoryUI();
-  saveEnabledCategories();
+    enabledCategories.pelajar = this.checked;
+    updateCategoryUI();
+    saveEnabledCategories();
 });
 
 document.getElementById('toggleUmum').addEventListener('change', function () {
-  enabledCategories.umum = this.checked;
-  updateCategoryUI();
-  saveEnabledCategories();
-});
-
-// Event listener untuk toggle subkategori
-document.querySelectorAll('.toggle-subcategory').forEach(checkbox => {
-  checkbox.addEventListener('change', function () {
-    const category = this.dataset.category;
-    const subcategory = this.dataset.subcategory;
-    enabledCategories.subcategories[category][subcategory] = this.checked;
+    enabledCategories.umum = this.checked;
     updateCategoryUI();
     saveEnabledCategories();
-  });
-});
-
-// Tombol disable semua subkategori pelajar
-document.getElementById('disableAllPelajarBtn').addEventListener('click', () => {
-  if (confirm('Apakah Anda yakin ingin menonaktifkan semua subkategori pelajar?')) {
-    Object.keys(enabledCategories.subcategories.pelajar).forEach(sub => {
-      enabledCategories.subcategories.pelajar[sub] = false;
-    });
-    updateCategoryUI();
-    saveEnabledCategories();
-    alert("Semua subkategori pelajar telah dinonaktifkan.");
-  }
-});
-
-// Tombol disable semua subkategori umum
-document.getElementById('disableAllUmumBtn').addEventListener('click', () => {
-  if (confirm('Apakah Anda yakin ingin menonaktifkan semua subkategori umum?')) {
-    Object.keys(enabledCategories.subcategories.umum).forEach(sub => {
-      enabledCategories.subcategories.umum[sub] = false;
-    });
-    updateCategoryUI();
-    saveEnabledCategories();
-    alert("Semua subkategori umum telah dinonaktifkan.");
-  }
 });
