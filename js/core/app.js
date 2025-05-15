472c0fbe6c7f870e08488e96da74b7c036639756
cendhanu <pergunusitubondo@gmail.com>
2025-05-15 22:26:55 +0700

    Update app.js

--- js/core/app.js
+++ js/core/app.js
@@ -6,3 +6,29 @@ document.addEventListener('DOMContentLoaded', () => {
 
   // Inisialisasi lainnya seperti stars, audio, event listener
 });
+
+let currentCategory = '';
+let currentSubcategory = '';
+let currentLevel = '';
+let score = 0;
+let quizStartTime = null;
+let musicEnabled = true;
+let soundEffectsEnabled = true;
+let quizMusicEnabled = true;
+let examCodePassword = 'KODEUJIAN12345';
+
+function formatName(name) {
+  return name.split(' ')
+            .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+            .join(' ');
+}
+
+function generateUniqueCode() {
+  const chars = 'ABCDEFGHJKLMNPQRSTUVWXYZ23456789';
+  let code = '';
+  for (let i = 0; i < 8; i++) {
+    if (i > 0 && i % 4 === 0) code += '-';
+    code += chars[Math.floor(Math.random() * chars.length)];
+  }
+  return code;
+}
