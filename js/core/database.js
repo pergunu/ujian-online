3edab9c6bae0a813f37a2f45632c7c70c7fa645d
cendhanu <pergunusitubondo@gmail.com>
2025-05-15 21:33:41 +0700

    Update database.js

--- js/core/database.js
+++ js/core/database.js
@@ -1,23 +1,21 @@
 const questions = {
-    pelajar: {
-        ipa: {
-            sd: [
-                {
-                    question: "Apa warna dari klorofil?",
-                    options: ["Hijau", "Merah", "Biru", "Kuning"],
-                    answer: 0,
-                    explanation: "Klorofil membuat daun tumbuhan hijau."
-                }
-            ],
-            smp: [],
-            sma: []
-        },
-        umum: {
-            logika: {
-                sd: [],
-                smp: [],
-                sma: []
-            }
-        }
+  pelajar: {
+    ipa: {
+      sd: [],
+      smp: [],
+      sma: []
+    },
+    ips: {
+      sd: [],
+      smp: [],
+      sma: []
     }
+  },
+  umum: {
+    logika: {
+      sd: [],
+      smp: [],
+      sma: []
+    }
+  }
 };
