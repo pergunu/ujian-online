af4a196f82a4797efbb2cc89237340e224ea078a
cendhanu <pergunusitubondo@gmail.com>
2025-05-16 22:31:43 +0700

    Update quiz.js

--- js/quiz.js
+++ js/quiz.js
@@ -17,3 +17,27 @@ function finishQuiz() {
 
   window.location.href = "result.html";
 }
+
+function generateCertificateCode(data, percentage) {
+  const now = new Date();
+  const dateStr = now.getDate().toString().padStart(2, '0') +
+                  (now.getMonth() + 1).toString().padStart(2, '0') +
+                  now.getFullYear();
+
+  const uniqueCode = Math.random().toString(36).substring(2, 10).toUpperCase();
+  const categoryText = data.status === "pelajar" ? "PELAJAR" : "UMUM";
+
+  let subcategoryText = "";
+  if (data.status === "pelajar") {
+    switch(data.pendidikan) {
+      case "SD": subcategoryText = "SD"; break;
+      case "SMP": subcategoryText = "SMP"; break;
+      case "SMA": subcategoryText = "SMA"; break;
+      case "SMK": subcategoryText = "SMK"; break;
+    }
+  } else {
+    subcategoryText = "UMUM";
+  }
+
+  return `${data.name.replace(/\s+/g, '')}/${categoryText}/${subcategoryText}/${dateStr}/${uniqueCode}/PERGUNU-STB`;
+}
