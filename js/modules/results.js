d6d4c11a194d34a326669595750d817904b08fe1
cendhanu <pergunusitubondo@gmail.com>
2025-05-16 14:46:47 +0700

    Update results.js

--- js/modules/results.js
+++ js/modules/results.js
@@ -1,57 +1,25 @@
-// js/modules/audio.js
-export const AudioManager = (() => {
-    let soundEffectsEnabled = true;
+// js/modules/results.js
+import { Config } from '../config.js';
+import { createConfetti } from '../utilities.js';
 
+export const Results = (() => {
     const init = () => {
-        const musicToggle = document.getElementById('musicToggle');
-        const quizMusic = document.getElementById('quizMusic');
-
-        if (musicToggle) {
-            musicToggle.addEventListener('click', () => {
-                soundEffectsEnabled = !soundEffectsEnabled;
-                if (!soundEffectsEnabled) {
-                    quizMusic.pause();
-                } else {
-                    quizMusic.play().catch(() => {});
-                }
-            });
-        }
-
-        // Aktifkan autoplay suara saat klik pertama kali
-        document.body.addEventListener('click', enableAudio, { once: true });
+        document.getElementById('printBtn').addEventListener('click', printCertificate);
+        document.getElementById('restartBtn').addEventListener('click', restartQuiz);
     };
 
-    const enableAudio = () => {
-        if (soundEffectsEnabled) {
-            playSound('openingSound');
-        }
+    const show = () => {
+        // Logika menampilkan hasil
     };
 
-    const playSound = (soundId) => {
-        if (!soundEffectsEnabled) return;
-        const sound = document.getElementById(soundId);
-        try {
-            const promise = sound.play();
-            if (promise !== undefined) {
-                promise.catch(error => {
-                    console.log("Autoplay prevented:", error);
-                });
-            }
-        } catch (error) {
-            console.log("Error playing sound:", error);
-        }
-    };
+    const printCertificate = () => window.print();
 
-    const playQuizMusic = () => {
-        const quizMusic = document.getElementById('quizMusic');
-        quizMusic.loop = true;
-        quizMusic.volume = 0.5;
-        quizMusic.play().catch(() => {});
+    const restartQuiz = () => {
+        location.reload();
     };
 
     return {
         init,
-        playSound,
-        playQuizMusic
+        show
     };
 })();
