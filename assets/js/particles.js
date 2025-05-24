2a91642d063d2c3999572dd43f7917921cbcca18
cendhanu <pergunusitubondo@gmail.com>
2025-05-24 13:46:33 +0700

    Update particles.js

--- assets/js/particles.js
+++ assets/js/particles.js
@@ -1,8 +1,19 @@
-// This file contains the configuration for the particles.js background
-// The configuration is actually loaded in the initParticles() function in main.js
-// This is just a placeholder file to show the structure
-
-// In a real application, you might want to externalize the configuration
-// or load different configurations for different screens
-
-console.log('Particles.js configuration loaded');
+// Konfigurasi Particles.js
+particlesJS("particles-js", {
+    "particles": {
+        "number": { "value": 80, "density": { "enable": true, "value_area": 800 } },
+        "color": { "value": "#ffffff" },
+        "shape": { "type": "circle" },
+        "opacity": { "value": 0.5, "random": true },
+        "size": { "value": 3, "random": true },
+        "line_linked": { "enable": true, "distance": 150, "color": "#ffffff", "opacity": 0.4, "width": 1 },
+        "move": { "enable": true, "speed": 2, "direction": "none", "random": true, "straight": false, "out_mode": "out" }
+    },
+    "interactivity": {
+        "detect_on": "canvas",
+        "events": {
+            "onhover": { "enable": true, "mode": "repulse" },
+            "onclick": { "enable": true, "mode": "push" }
+        }
+    }
+});
