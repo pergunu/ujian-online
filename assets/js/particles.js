e72cd1c67c96a2007fe41ab4189076d53ee55d23
cendhanu <pergunusitubondo@gmail.com>
2025-05-24 12:50:04 +0700

    Create particles.js

---
+++ assets/js/particles.js
@@ -0,0 +1,8 @@
+// This file contains the configuration for the particles.js background
+// The configuration is actually loaded in the initParticles() function in main.js
+// This is just a placeholder file to show the structure
+
+// In a real application, you might want to externalize the configuration
+// or load different configurations for different screens
+
+console.log('Particles.js configuration loaded');
