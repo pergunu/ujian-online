86de62f3120c980c9258438c7f1818d8ed06af66
cendhanu <pergunusitubondo@gmail.com>
2025-05-28 15:17:15 +0700

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,6 +1,6 @@
 // File ini JANGAN di-commit ke GitHub (tambahkan ke .gitignore)
 const APP_CONFIG = {
-  AI_API_KEY: 'sk-YourActualAPIKeyHere', // API Key OpenAI atau layanan AI lainnya
+  AI_API_KEY: 'sk-8837b24327bd4db99e36688951ceaea8', // API Key OpenAI atau layanan AI lainnya
   ADMIN_PASS: '65614222', // Password admin default
   LOGIN_CODE: '12345' // Kode login default
 };
