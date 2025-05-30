84afe08eed2649780912355a330aab910164b54d
cendhanu <pergunusitubondo@gmail.com>
2025-05-30 22:15:15 +0700

    Update particles.js

--- assets/js/particles.js
+++ assets/js/particles.js
@@ -1,9 +1,8 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Konfigurasi partikel yang lebih sederhana
+// particles.js configuration
 particlesJS("particles-js", {
   "particles": {
     "number": {
-      "value": 40,
+      "value": 80,
       "density": {
         "enable": true,
         "value_area": 800
@@ -17,13 +16,16 @@ particlesJS("particles-js", {
       "stroke": {
         "width": 0,
         "color": "#000000"
+      },
+      "polygon": {
+        "nb_sides": 5
       }
     },
     "opacity": {
       "value": 0.5,
       "random": true,
       "anim": {
-        "enable": false,
+        "enable": true,
         "speed": 1,
         "opacity_min": 0.1,
         "sync": false
@@ -33,14 +35,17 @@ particlesJS("particles-js", {
       "value": 3,
       "random": true,
       "anim": {
-        "enable": false
+        "enable": true,
+        "speed": 2,
+        "size_min": 0.1,
+        "sync": false
       }
     },
     "line_linked": {
       "enable": true,
       "distance": 150,
       "color": "#ffffff",
-      "opacity": 0.2,
+      "opacity": 0.4,
       "width": 1
     },
     "move": {
@@ -52,7 +57,9 @@ particlesJS("particles-js", {
       "out_mode": "out",
       "bounce": false,
       "attract": {
-        "enable": false
+        "enable": true,
+        "rotateX": 600,
+        "rotateY": 1200
       }
     }
   },
@@ -60,12 +67,40 @@ particlesJS("particles-js", {
     "detect_on": "canvas",
     "events": {
       "onhover": {
-        "enable": false
+        "enable": true,
+        "mode": "grab"
       },
       "onclick": {
-        "enable": false
+        "enable": true,
+        "mode": "push"
       },
       "resize": true
+    },
+    "modes": {
+      "grab": {
+        "distance": 140,
+        "line_linked": {
+          "opacity": 1
+        }
+      },
+      "bubble": {
+        "distance": 400,
+        "size": 40,
+        "duration": 2,
+        "opacity": 8,
+        "speed": 3
+      },
+      "repulse": {
+        "distance": 200,
+        "duration": 0.4
+      },
+      "push": {
+        "particles_nb": 4
+      },
+      "remove": {
+        "particles_nb": 2
+      }
     }
-  }
+  },
+  "retina_detect": true
 });
